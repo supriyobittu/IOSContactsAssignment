20db1c8077b4686fd99efa8333088c7d6fdb8334
Supriyo Mondal <mondalsupriyo111@gmail.com>
2020-02-29 20:27:25 +0530

    Added initial View Controller

---
+++ Contacts/Contacts/ContactList/Controllers/ContactListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ContactListViewController.swift
+//  Contacts
+//
+//  Created by Supriyo Mondal on 29/02/20.
+//  Copyright © 2020 Supriyo Mondal. All rights reserved.
+//
+
+import UIKit
+
+class ContactListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
